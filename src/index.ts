7b3396ef6de4ce323f0714801191c3e7b8caf47c
lrlineroa <leonardoraulinero@gmail.com>
2021-12-26 05:05:41 -0500

    refactor clean

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,15 @@
-import { DataModelType, ModelType } from "src/@types/datamodel";
 import {
   getFileContents,
-  getFilesFromMigrations,
-  saveGeneratedDataModel,
 } from "@utils/fileSystem";
-import { getDataModel } from "./datamodel";
 import {
-  generateApiRestFromMigration,
   generateHttpRequestsFromKnexModels,
-  generateMigrationFromSQLModel,
-  generateModelFromMigration,
   generateModelsFromKnexModels,
-  generateTypesFromMigrations,
   generatetApiRestFromKnexModels,
+  generateTypesFromKnexModels,
 } from "./generator";
 import { KnexModelsType } from "src/@types";
 import { generated_knex_models_location } from "./constants/locations";
 
-// //read the migration file from the disk
-// const migrationFiles = getFilesFromMigrations();
-// for (let fileName of migrationFiles) {
 let knexModels: KnexModelsType = JSON.parse(
   getFileContents(generated_knex_models_location, "knex_models.json")
 );
@@ -43,17 +33,3 @@ try {
 } catch (error: unknown) {
   console.error(error);
 }
-//   try {
-//     generateApiRestFromMigration(fileName);
-//   } catch (error) {
-//     console.log("error at generate API REST: " + fileName);
-//     console.log(error);
-//   }
-// }
-
-// try {
-//   generateTypesFromMigrations(migrationFiles);
-// } catch (error) {
-//   console.log("error : at generating Types");
-//   console.log(error);
-// }
